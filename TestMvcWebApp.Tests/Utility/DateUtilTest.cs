7cf4d58f8654441554c4dcf936d4de7032c03029
Rishabh <devileatspie@gmail.com>
2020-07-25 03:24:52 +0530

    initial commit

---
+++ TestMvcWebApp.Tests/Utility/DateUtilTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestMvcWebApp.Utility;
+
+namespace TestMvcWebApp.Tests.Utility
+{
+    [TestClass]
+    public class DateUtilTest
+    {
+        [TestMethod]
+        public void GetCurrentDate_IsNotNull()
+        {
+            Assert.IsNotNull(DateUtil.GetCurrentDate());
+        }
+
+        [TestMethod]
+        public void GetCurrentTime_IsNotNull()
+        {
+            Assert.IsNotNull(DateUtil.GetCurrentTime());
+        }
+    }
+}
---
+++ TestMvcWebApp/Utility/DateUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestMvcWebApp.Utility
+{
+    public static class DateUtil
+    {
+        public static string GetCurrentDate()
+        {
+            return DateTime.Today.ToShortDateString();
+        }
+
+        public static string GetCurrentTime()
+        {
+            return DateTime.Now.ToShortTimeString();
+        }
+    }
+}
